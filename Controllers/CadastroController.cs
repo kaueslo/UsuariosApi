11224d3f2ba1200fa8186a0b6af05edc45ac29f3
Kaue Sloboda <kaue.sloboda@hotmail.com>
2023-03-09 21:54:16 -0300

    adicionando as primeiras logicas do usuariosApi

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UsuariosApi.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class CadastroController : ControllerBase
+	{
+		[HttpPost("CadastraUsuario")]
+		public IActionResult CadastraUsuario(CreateUsuarioDto createDto)
+		{
+			//TODO chamar o service
+			return Ok();
+		}
+	}
+}
---
+++ Data/Dto/CreateUsuarioDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsuariosApi.Data.Dto
+{
+	public class CreateUsuarioDto
+	{
+		[Required(ErrorMessage ="É necessário informar o nome do usuário")]
+		public string UserName { get; set; }
+		[Required(ErrorMessage ="É necessário informar o E-mail")]
+		public string Email { get; set; }
+		[Required(ErrorMessage ="É necessário informar a senha")]
+		public string Password { get; set; }
+	}
+}
